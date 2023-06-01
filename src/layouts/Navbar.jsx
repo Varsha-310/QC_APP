e4a3b11fd7d2d2f221c0eac37149229ef82a992e
prabhat <prabhat.prasad_conslt@qwikcilver.com>
2023-06-01 17:19:18 +0530

    Navbar fix #1

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import SearchIcon from "../assets/icons/svgs/magnifier.svg"
+import DownArrow from "../assets/icons/svgs/DownArr.svg"
+import UserIcon from "../assets/icons/svgs/user.svg"
 import "./styles/Navbar.css";
 
 const Navbar = () => {
@@ -12,8 +15,8 @@ const Navbar = () => {
       
       <div className="navbar-menu">
         <ul>
-          <li className="navbar-menu-item"></li>
-          <li className="navbar-menu-item"></li>
+          <li className="navbar-menu-item"><img src={SearchIcon} alt=""/></li>
+          <li className="navbar-menu-item"><img src={UserIcon} alt=""/><img src={DownArrow} alt=""/></li>
         </ul>
       </div>
     </div>
