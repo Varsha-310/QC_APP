f811342aef9d44c3d7a2b9b396cb591725b60918
bkmahapatra <batakrushna@marmeto.com>
2023-06-21 12:25:23 +0530

    gift card list update

--- src/components/DataTable/GiftCardTable.jsx
+++ src/components/DataTable/GiftCardTable.jsx
@@ -1,31 +1,30 @@
 import React from "react";
 import "./styles/GiftCardTable.css";
 import { Link } from "react-router-dom";
+import GiftcardImage from "../../assets/images/dummyGiftcardImage.png";
 import EditIcon from "../../assets/icons/svgs/Edit.svg";
 import DeleteIcon from "../../assets/icons/svgs/deletefill.svg";
 import DetailIcon from "../../assets/icons/svgs/Msg.svg";
 
 const GiftCardTable = ({ data }) => {
   return (
-    <div className="gift-cards-table">
+    <table className="gift-cards-table">
       <thead>
         <tr>
           <th>Gift Card</th>
           <th>Image</th>
           <th>Date</th>
-          <th>Gift card launch date</th>
           <th>Action</th>
         </tr>
       </thead>
       <tbody>
-        {data.map((row) => (
-          <tr>
-            <td>{row.gift_card}</td>
+        {data?.map((row,index) => (
+          <tr key={index}>
+            <td>{row.title}</td>
             <td className="gc-table__image">
-              <img src={row.image} alt="" />
+              <img src={row?.images[0]?.src} alt="" />
             </td>
-            <td>{row.date}</td>
-            <td>{row.gift_card_launch_date}</td>
+            <td>{row.created_at.slice(0,10)}</td>
             <td className="gc-table__actions">
                 <Link><img className="gc-table-icons" src={DetailIcon} alt=""/></Link>
                 <Link><img className="gc-table-icons" src={EditIcon} alt=""/></Link>
@@ -34,7 +33,7 @@ const GiftCardTable = ({ data }) => {
           </tr>
         ))}
       </tbody>
-    </div>
+    </table>
   );
 };
 
