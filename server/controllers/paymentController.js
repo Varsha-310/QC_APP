35b21a036e85d29d6d40f305b88cbaee461bd1f1
Anubhav Gupta <anubhav.gupta_conslt@pinelabs.com>
2023-08-11 13:18:32 +0530

    changes

--- server/controllers/paymentController.js
+++ server/controllers/paymentController.js
@@ -130,6 +130,6 @@ const updateBillingHistory = async (data) => {
         { $set: { "plan.plan_name": data.lastname } }
       );
     await store.findOneAndUpdate({email : data.email, mandate : data});
-    await generateCSV();
+    // await generateCSV();
     return 1;
 };
