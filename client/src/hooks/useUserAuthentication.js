4bcc83ac4cfc4ef7a75b518cc26b8372c492f435
prabhat <prabhat.prasad_conslt@qwikcilver.com>
2023-08-23 11:40:30 +0530

    user authentication update

--- client/src/hooks/useUserAuthentication.js
+++ client/src/hooks/useUserAuthentication.js
@@ -1,14 +1,46 @@
 import { useEffect } from "react";
-import { setUserToken } from "../utils/userAuthenticate";
+import { setUserStatus, setUserToken } from "../utils/userAuthenticate";
+import instance from "../axios";
 
 const useUserAuthentication = () => {
+  const getKycStatus = async (token) => {
+    const url = "/kyc/status";
+    const headers = {
+      Authorization: token,
+    };
+
+    try {
+      const res = await instance.post(url, {}, { headers });
+
+      if (res?.status === 200) {
+        const resData = res.data;
+
+        return await resData.data;
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+    return false;
+  };
+
+  const setUserCredentials = async (token) => {
+    setUserToken(token);
+
+    const active_status = await getKycStatus(token);
+
+    if (active_status?.dashboard_activated === true) {
+      setUserStatus(true);
+    }
+    // console.log("activestatus", active_status);
+  };
+
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
     const token = urlParams.get("token");
 
     if (token) {
-      setUserToken(token);
-      sessionStorage.setItem("qcUserStatus", true);
+      setUserCredentials(token);
+      // sessionStorage.setItem("qcUserStatus", true);
     }
   }, []);
 };
