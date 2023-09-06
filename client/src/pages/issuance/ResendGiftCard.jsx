0e2bdc0bba6bc80f42a9bc3bfec3d25bbfabe16e
bkmahapatra <batakrushna@marmeto.com>
2023-09-06 14:38:18 +0530

    resend filter update

--- client/src/pages/issuance/ResendGiftCard.jsx
+++ client/src/pages/issuance/ResendGiftCard.jsx
@@ -1,24 +1,33 @@
 import React, { useEffect, useState } from "react";
 import ResendGiftCardTable from "../../components/DataTable/ResendGiftCardTable";
-import axios from "axios";
 import instance from "../../axios";
-import { createPortal } from "react-dom";
-import Spinner from "../../components/Loaders/Spinner";
 import Pagination from "../../components/Pagination";
 import { getUserToken } from "../../utils/userAuthenticate";
 import BarLoading from "../../components/Loaders/BarLoading";
+import { BiSearch, BiFilter } from "react-icons/bi";
+import { MdClear } from "react-icons/md";
+import { DatePicker, Input } from "antd";
+
+const { RangePicker } = DatePicker;
 
 const ResendGiftCard = () => {
+  const PER_PAGE_ITEM = 15;
+
   const [orders, setOrders] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
   const [isLoading, setIsLoading] = useState(false);
-  // console.log(orders);
+
+  // for search
+  const [searchTerm, setSearchTerm] = useState("");
+  const [selectedDateRange, setSelectedDateRange] = useState(null); // Add this state
+  const [filteredOrders, setFilteredOrders] = useState(null);
+  console.log(selectedDateRange);
 
   // fetch orders
   const fetchOrders = async () => {
     setIsLoading(true);
 
-    const url = `/giftcard/orders?page=${currentPage}&pageSize=10`;
+    const url = `/giftcard/orders?page=${currentPage}&pageSize=${PER_PAGE_ITEM}`;
     const headers = {
       Authorization: getUserToken(),
     };
@@ -68,17 +77,51 @@ const ResendGiftCard = () => {
     }
   };
 
+  const handleSearch = async () => {
+    setIsLoading(true);
+
+    let url = `/giftcard/orders?page=1&pageSize=1`;
+
+    if (searchTerm) {
+      url += `&orderNo=${searchTerm}`;
+    }
+
+    // Append date range to the URL if it exists
+    if (selectedDateRange) {
+      url += `&startDate=${selectedDateRange[0].format(
+        "YYYY-MM-DD"
+      )}&endDate=${selectedDateRange[1].format("YYYY-MM-DD")}`;
+    }
+
+    const headers = {
+      Authorization: getUserToken(),
+    };
+
+    try {
+      const res = await instance.post(url, {}, { headers });
+      const resData = res.data;
+
+      if (resData.code === 200) {
+        setFilteredOrders(resData);
+      }
+    } catch (error) {
+      if (error.resonpose) {
+        console.error("server error", error.resonpose.data);
+      } else if (error.request) {
+        console.error("no response receieved", error.request);
+      } else {
+        console.error("Error", error.message);
+      }
+    } finally {
+      setSearchTerm(null);
+      setSelectedDateRange(null);
+      setIsLoading(false);
+    }
+  };
+
   useEffect(() => {
     fetchOrders();
   }, [currentPage]);
-
-  if (isLoading) {
-    return (
-      <div className="component">
-        <BarLoading />
-      </div>
-    );
-  }
 
   return (
     <div className="component">
@@ -86,21 +129,83 @@ const ResendGiftCard = () => {
         <div className="section-box-title">Gift Card Orders</div>
       </div>
 
-      {!!orders && orders?.total !== 0 ? (
-        <>
-          <ResendGiftCardTable data={orders?.data} resendMail={resendMail} />
+      <div className="app-table__list-actions">
+        <Input
+          type="number"
+          name="orderNumber"
+          id="orderNumber"
+          className="app-table__action-fields"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Order Number"
+        />
+        <RangePicker
+          size="middle"
+          className="app-table__action-fields"
+          value={selectedDateRange}
+          onChange={(dates) => setSelectedDateRange(dates)}
+        />
 
-          <Pagination
-            total={orders?.total}
-            perPage={10}
-            setCurrentPage={setCurrentPage}
-            currentPage={currentPage}
-          />
-        </>
+        <BiSearch className="app-table__action-icons" onClick={handleSearch} />
+
+        <MdClear
+          className="app-table__action-icons"
+          onClick={() => setFilteredOrders(null)}
+        />
+
+        {/* <BiFilter className="app-table__action-icons" onClick={() => {}} /> */}
+      </div>
+
+      {isLoading === false ? (
+        filteredOrders ? (
+          // Display search results
+          <>
+            {filteredOrders?.total >= 1 ? (
+              <>
+                <ResendGiftCardTable
+                  data={filteredOrders?.data}
+                  resendMail={resendMail}
+                />
+
+                <Pagination
+                  total={filteredOrders?.total}
+                  perPage={PER_PAGE_ITEM}
+                  setCurrentPage={setCurrentPage}
+                  currentPage={currentPage}
+                />
+              </>
+            ) : (
+              <div className="section-box-container">
+                <div className="section-box-title">No matching results</div>
+              </div>
+            )}
+          </>
+        ) : (
+          // Display normal list
+          <>
+            {orders && orders.total !== 0 ? (
+              <>
+                <ResendGiftCardTable
+                  data={orders.data}
+                  resendMail={resendMail}
+                />
+
+                <Pagination
+                  total={orders.total}
+                  perPage={10}
+                  setCurrentPage={setCurrentPage}
+                  currentPage={currentPage}
+                />
+              </>
+            ) : (
+              <div className="section-box-container">
+                <div className="section-box-title">There are no Gift cards</div>
+              </div>
+            )}
+          </>
+        )
       ) : (
-        <div className="section-box-container">
-          <div className="section-box-title"> There is 0 Gift cards</div>
-        </div>
+        <BarLoading />
       )}
     </div>
   );
