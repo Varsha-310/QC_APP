a86d88e733561450cb9368e7740549f9cccc8f40
Anubhav Gupta <anubhav.gupta_conslt@pinelabs.com>
2023-09-13 19:05:31 +0530

    removed encrypting qc cred

--- server/models/qcCredentials.js
+++ server/models/qcCredentials.js
@@ -18,38 +18,5 @@ const qc_credentials = mongoose.Schema({
 });
 
 
-/**
- * pre hook to encrypt data befor storing DB
- */
-qc_credentials.pre("save", function (next) {
-  const secretKey = process.env.ENCRYPT_KEY;
-  const encryptedPassword = CryptoJS.AES.encrypt(this.password, secretKey).toString();
-  const encryptedUsername = CryptoJS.AES.encrypt(this.username, secretKey).toString();
-
-  this.password = encryptedPassword;
-  this.username = encryptedUsername;
-
-  next();
-});
-
-/**
- * post hook to decrypt data while fetching from DB
- */
-qc_credentials.post("findOne", function (docs) {
-  const documents = Array.isArray(docs) ? docs : [docs];
-
-  documents.forEach((doc) => {
-    const secretKey = process.env.ENCRYPT_KEY;
-
-    const decryptedBytesOfPassword = CryptoJS.AES.decrypt(doc.password, secretKey);
-    const decryptedBytesOfUsername = CryptoJS.AES.decrypt(doc.username, secretKey);
-
-    const decryptedPassword = decryptedBytesOfPassword.toString(CryptoJS.enc.Utf8);
-    const decryptedUsername = decryptedBytesOfUsername.toString(CryptoJS.enc.Utf8);
-
-    doc.password = decryptedPassword;
-    doc.username = decryptedUsername
-  });
-});
 
 export default mongoose.model("qc_credentials", qc_credentials);
