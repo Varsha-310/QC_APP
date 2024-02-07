abcf5d4d0252db64c9c79f93ce2320c7086f393f
Anubhav Gupta <anubhav.gupta_conslt@pinelabs.com>
2024-02-07 10:49:00 +0530

    changes in logger file

--- server/helper/logger.js
+++ server/helper/logger.js
@@ -1,18 +1,20 @@
 import log4js from 'log4js';
 import path from 'path';
 
+
+const rightNow = new Date();
+const res = rightNow.toISOString().slice(0, 10).replace(/-/g, "_");
 // Log4js configuration
 log4js.configure({
   appenders: {
     console: { type: 'console' },
     file: {
-        "type": "dateFile",
-        "filename": "logs/app",
-        "pattern": ".yyyy-MM-dd",
-        "layout": {
-          "type": "pattern",
-          "pattern": "%d{ISO8601_WITH_TZ_OFFSET} [%p] %c - %m%n"
-        }
+        "type": "file",
+        "filename": `logs/${res}.log`
+        ,
+        "maxLogSize": 10485760,
+        "backups": 3,
+        "category": "app"
     }
   },
   categories: {
